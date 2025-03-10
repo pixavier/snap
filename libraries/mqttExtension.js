e178d3ac5083d6100a22daba40e6f61611c73786
Xavier Pi <63783572+pixavier@users.noreply.github.com>
2025-03-10 22:48:58 +0100

    Update mqttExtension.js

--- libraries/mqttExtension.js
+++ libraries/mqttExtension.js
@@ -402,10 +402,6 @@ SnapExtensions.primitives.set(
             proc.pushContext('doYield');
             proc.pushContext();
         } else {
-			var pos = b64.indexOf(',');
-			if (pos >= 0) {
-				b64 = b64.substring(pos + 1);
-			}
             return window.atob(b64);
         }
     }
